2958b2b699912f17e4ad84b4109d03af34daf59e
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-01-05 13:57:02 +0100

    start JS project

---
+++ start.js
@@ -0,0 +1,11 @@
+var randomNumber = Math.floor(Math.random() * 100) + 1;
+
+var guesses = document.querySelector(".guesses");
+var lastResult = document.querySelector(".lastResult");
+var lowOrHi = document.querySelector(".lowOrHi");
+
+var guessSubmit = document.querySelector(".guessSubmit");
+var guessField = document.querySelector(".guessField");
+
+var guessCount = 1;
+var resetButton;
