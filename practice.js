a64647c3bb2de37401eaad1b108312c5893bd2eb
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-03-13 12:25:37 +0100

    DOM

--- practice.js
+++ practice.js
@@ -293,10 +293,29 @@
 
 // console.log(occ);
 
-const users = { name: "Mango", age: 2, hobby: "html", premium: true };
+// const users = { name: "Mango", age: 2, hobby: "html", premium: true };
 
-function convertHashToArray(hash) {
-  return Object.entries(hash);
+// function convertHashToArray(hash) {
+//   return Object.entries(hash);
+// }
+
+const select = document.querySelector(".pizza-select");
+const textOutput = document.querySelector(".text-output");
+const valueOutput = document.querySelector(".value-output");
+
+setOutput();
+
+select.addEventListener("change", setOutput);
+
+function setOutput() {
+  console.log(select.options);
+  const selectedOptionValue = select.value;
+  console.log(selectedOptionValue);
+  const selectedOptionIndex = select.selectedIndex;
+  console.log(selectedOptionIndex);
+  const selectedOptionText = select.options[selectedOptionIndex].text;
+  console.log(selectedOptionText);
+
+  textOutput.textContent = selectedOptionText;
+  valueOutput.textContent = selectedOptionValue;
 }
-
-console.dir(convertHashToArray(users));
