f0c13e152dd643545010e6639dc65d2a7de45b9f
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-03-20 09:27:46 +0100

    Lodash use

--- practice.js
+++ practice.js
@@ -462,23 +462,26 @@
 
 // document.body.appendChild(fragment);
 
-const parent = document.querySelector("#parent");
-const child = document.querySelector("#child");
-const descendant = document.querySelector("#descendant");
+// const parent = document.querySelector("#parent");
+// const child = document.querySelector("#child");
+// const descendant = document.querySelector("#descendant");
 
-parent.addEventListener("click", () => {
-  alert(
-    "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-  );
-});
+// parent.addEventListener("click", () => {
+//   alert(
+//     "Parent click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
 
-child.addEventListener("click", () => {
-  alert(
-    "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
-  );
-});
+// child.addEventListener("click", () => {
+//   alert(
+//     "Child click handler. This alert will not appear when clicking on Descendant, the event will not reach here!"
+//   );
+// });
 
-descendant.addEventListener("click", (event) => {
-  event.stopPropagation();
-  alert("Descendant click handler");
-});
+// descendant.addEventListener("click", (event) => {
+//   event.stopPropagation();
+//   alert("Descendant click handler");
+// });
+
+const result = _.add(2, 3);
+console.log(result); // 5
