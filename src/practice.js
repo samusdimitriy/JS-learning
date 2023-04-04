e90efbcc13f66653aec1e22993c068d37ae90560
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-04-04 19:03:59 +0200

    setInterval, timer

--- src/practice.js
+++ src/practice.js
@@ -575,4 +575,27 @@
 //     console.error("Remove state error: ", error.message);
 //   }
 // };
-import Parcel from "parcel";
+// import Parcel from "parcel";
+
+// const greet = () => {
+//   console.log("Hello!");
+// };
+
+// const timerId = setTimeout(greet, 3000);
+
+// clearTimeout(timerId);
+
+// const startBtn = document.querySelector(".js-start");
+// const stopBtn = document.querySelector(".js-stop");
+// let timerId = null;
+
+// startBtn.addEventListener("click", () => {
+//   timerId = setInterval(() => {
+//     console.log(`I love async JS!  ${Math.random()}`);
+//   }, 1000);
+// });
+
+// stopBtn.addEventListener("click", () => {
+//   clearInterval(timerId);
+//   console.log(`Interval with id ${timerId} has stopped!`);
+// });
